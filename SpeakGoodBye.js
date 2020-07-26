43b74488e7567dfc32293b50e89fd745c38dc743
emovladinov <67592028+emovladinov@users.noreply.github.com>
2020-07-26 19:43:16 +0300

    Create SpeakGoodBye.js

---
+++ SpeakGoodBye.js
@@ -0,0 +1,7 @@
+(function () {
+    var byeSpeaker={speak:function speak(name) {
+      console.log(speakWord + " " + name);
+}}
+    var speakWord = "Good Bye";
+    window.byeSpeaker = byeSpeaker;
+})(window);
